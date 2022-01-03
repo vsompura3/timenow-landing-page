48996bc29cede17b706b6bd25741a378948845eb
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-01-03 20:17:00 +0530

    Home Page complete

---
+++ notes.md
@@ -0,0 +1,18 @@
+# Colors
+
+- #1D1B67 - midnight
+- #46CCDE - seaform blue
+
+- #425466 - slate
+
+- #CDF9FF - lavender
+
+- #3B60BF - blue
+
+- #005661 - bubble green
+
+- #A8A8A8 - gray
+
+- #3A84F7 -copper rust
+
+- #ffffff -white
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+      midnight: '#1D1B67',
+      gray: '#A8A8A8',
+      blue: '#3A84F7',
+      slate: '#425466',
+      lavender: '#CDF9FF',
+      'tahiti-blue': '#46CCDE',
+      'copper-rust': '#3B60BF',
+      'bubble-green': '#005661',
+    },
+    fontFamily: {
+      inter: 'Inter, Arial, Helvetica, sans-serif',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
